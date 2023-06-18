9a06a074e6495552885d9391605e79d6d7019d7d
Igors Saters <s21sateigor@venta.lv>
2023-06-18 22:02:14 +0300

    Branch merge from 'igors'

--- src/main/java/lv/venta/fitness/models/Meal.java
+++ src/main/java/lv/venta/fitness/models/Meal.java
@@ -1,2 +1,40 @@
-package lv.venta.fitness.models;public class Meal {
+package lv.venta.fitness.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+@Table(name = "meal_table")
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Meal {
+    @Column(name = "idme")
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Setter(value = AccessLevel.NONE)
+    private long idme;
+
+    @Column(name = "title")
+    @NotNull
+    @Size(min = 3, max = 20)
+    private String title;
+
+    @Column(name = "description")
+    @NotNull
+    @Size(min = 3, max = 30)
+    private String description;
+
+    @ToString.Exclude
+    private Collection<Ingredient> ingredientCollection = new ArrayList<>();
+
+    public Meal(String title, String description) {
+        this.title = title;
+        this.description = description;
+    }
 }
