213916f99231123d5d080663fdf1a6e0d4ecbef9
NiklavsBergs <bergs.niklavs@gmail.com>
2023-06-18 22:11:09 +0300

    updated excercise

---
+++ src/main/java/lv/venta/fitness/models/Exercise.java
@@ -0,0 +1,79 @@
+package lv.venta.fitness.models;
+
+import java.util.ArrayList;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Table(name = "excercise_table")
+@MappedSuperclass
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class Exercise {
+	
+	@Column(name = "Ide")
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Setter(value = AccessLevel.NONE)
+	private long ide;
+	
+	@Column(name = "Title")
+	@Size(min = 3, max = 30)
+	@Pattern(regexp = "[A-Z]{1}[a-z\\ ]+", message = "Only latin letters")
+	private String title;
+	
+	@Column(name = "Description")
+	@Size(min = 3, max = 100)
+	@Pattern(regexp = "[A-Z]{1}[a-z\\ ]+", message = "Only latin letters")
+	private String description;
+	
+	@Column(name = "RestInterval")
+	@Min(0)
+	@Max(10)
+	private float restInterval;
+	
+	@Column(name = "Repetitions")
+	@Min(1)
+	@Max(100)
+	private int repetitions;
+	
+	@Column(name = "TargetMuscles")
+	@NotNull
+	private ArrayList<String> targetMuscles;
+	
+	@Column(name = "AddedWeight")
+	@Min(1)
+	@Max(1000)
+	private float addedWeight;
+
+	public Exercise(
+			@Size(min = 3, max = 30) @Pattern(regexp = "[A-Z]{1}[a-z\\ ]+", message = "Only latin letters") String title,
+			@Size(min = 3, max = 100) @Pattern(regexp = "[A-Z]{1}[a-z\\ ]+", message = "Only latin letters") String description,
+			@Min(0) @Max(10) float restInterval, @Min(1) @Max(100) int repetitions,
+			@NotNull ArrayList<String> targetMuscles, @Min(1) @Max(1000) float addedWeight) {
+		this.title = title;
+		this.description = description;
+		this.restInterval = restInterval;
+		this.repetitions = repetitions;
+		this.targetMuscles = targetMuscles;
+		this.addedWeight = addedWeight;
+	}
+	
+	
+}
