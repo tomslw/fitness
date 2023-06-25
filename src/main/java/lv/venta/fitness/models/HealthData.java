40470e2d7cf84f15cf236abbb8de7ba69e0bea00
NiklavsBergs <bergs.niklavs@gmail.com>
2023-06-25 19:27:13 +0300

    Added User

--- src/main/java/lv/venta/fitness/models/HealthData.java
+++ src/main/java/lv/venta/fitness/models/HealthData.java
@@ -9,6 +9,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Max;
@@ -64,5 +66,9 @@ public class HealthData {
 	@Column(name = "date")
 	@NotNull
 	private LocalDateTime date;
+	
+	@ManyToOne
+	@JoinColumn(name="idus")
+	private User user;
 	
 }
---
+++ src/main/java/lv/venta/fitness/models/User.java
@@ -0,0 +1,59 @@
+package lv.venta.fitness.models;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Table(name = "user_table")
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class User {
+	
+	@Column(name = "idus")
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Setter(value = AccessLevel.NONE)
+	private long idus;
+	
+	@Column(name = "Name")
+	@Size(min = 3, max = 30)
+	@Pattern(regexp = "[A-Z]{1}[a-z\\ ]+", message = "Only latin letters")
+	@NotNull
+	private String name;
+	
+	@Column(name = "Surname")
+	@Size(min = 3, max = 30)
+	@Pattern(regexp = "[A-Z]{1}[a-z\\ ]+", message = "Only latin letters")
+	@NotNull
+	private String surname;
+	
+	@Column(name = "Username")
+	@Size(min = 3, max = 30)
+	@Pattern(regexp = "[A-Z,a-z,0-9,_\\ ]+", message = "Only latin letters")
+	@NotNull
+	private String username;
+	
+	private String password;
+	
+	@OneToMany(mappedBy="HealthData")
+	@ToString.Exclude
+	@NotNull
+	private Collection<HealthData> allHealthData;
+}
