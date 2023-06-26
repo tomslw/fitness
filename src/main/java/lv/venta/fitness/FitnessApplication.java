2535a3b53c40340fff73e5c4f15ba85a5813565e
Igors Saters <s21sateigor@venta.lv>
2023-06-26 18:44:43 +0300

    Add: ExerciseController, templates

--- src/main/java/lv/venta/fitness/FitnessApplication.java
+++ src/main/java/lv/venta/fitness/FitnessApplication.java
@@ -1,7 +1,15 @@
 package lv.venta.fitness;
 
+import lv.venta.fitness.models.Excersise;
+import lv.venta.fitness.repos.ExcersiseRepo;
+import lv.venta.fitness.services.IExcersiseService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 @SpringBootApplication
 public class FitnessApplication {
@@ -10,4 +18,14 @@ public class FitnessApplication {
 		SpringApplication.run(FitnessApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner testModel(ExcersiseRepo excersiseRepo){
+		return new CommandLineRunner() {
+			@Override
+			public void run(String... args) throws Exception {
+				Excersise excersise = new Excersise("Wanking", "Wanking with hand hard", 4, 4, new ArrayList<>(Arrays.asList("bicpes", "triceps")), 200);
+				excersiseRepo.save(excersise);
+			}
+		};
+	}
 }
--- src/main/java/lv/venta/fitness/controllers/ExcersiseController.java
+++ src/main/java/lv/venta/fitness/controllers/ExcersiseController.java
@@ -1,16 +1,77 @@
 package lv.venta.fitness.controllers;
 
 
+import jakarta.validation.Valid;
+import lv.venta.fitness.models.Excersise;
+import lv.venta.fitness.services.IExcersiseService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
-public class AllController {
+public class ExcersiseController {
+
+
+    private IExcersiseService excersiseService;
 
     @GetMapping("/error")
     public String getError(Model model){
         model.addAttribute("packetError", "Error");
         return "error-page";
     }
+
+    @GetMapping("/exercise/showAll")
+    public String getAllExercises(Model model){
+        model.addAttribute("exercises", excersiseService.selectAllExcersises());
+        return "all-exercises-page";
+    }
+
+    @GetMapping("/exercise/showExerciseByMuscle/{muscle}")
+    public String getExerciseByMuscle(@PathVariable(name = "muscle") String muscle, Model model){
+        try {
+            model.addAttribute("exercise", excersiseService.selectExcersisesByMuscle(muscle));
+            return "specific-exercise-page";
+        } catch (Exception e) {
+            model.addAttribute("packetError", e.getMessage());
+            return "error-page";
+        }
+    }
+
+    @GetMapping("/exercise/delete/{id}")
+    public String getDeleteExercise(@PathVariable(name = "id") long id, Model model){
+        try{
+            excersiseService.deleteExcersiseById(id);
+            model.addAttribute("room", excersiseService.selectAllExcersises());
+            return "all-exercises-page";
+        } catch (Exception e) {
+            model.addAttribute("packetError", e.getMessage());
+            return "error-page";
+        }
+    }
+
+    @GetMapping("/exercise/insertNewExercise")
+    public String getAddExercise(Model model){
+        model.addAttribute("exercise", new Excersise());
+        return "add-exercise-page";
+    }
+
+    @PostMapping("/exercise/insertNewExercise")
+    public String postAddExercise(@Valid Excersise excersise, BindingResult result) {
+        if(!result.hasErrors()) {
+            try {
+                excersiseService.insertNewExcersise(excersise.getTitle(), excersise.getDescription(), excersise.getRestInterval(), excersise.getRepetitions(), excersise.getTargetMuscles(), excersise.getAddedWeight());
+                return "redirect:/exercise/showAll";
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+                return "error-page";
+            }
+        } else {
+            return "add-exercise-page";
+        }
+    }
+
 }
