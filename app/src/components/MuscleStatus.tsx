211481bc3165eea6c8766e63d5da77050ffa36d1
Toms Strals <stralstomslw@gmail.com>
2023-06-27 15:22:21 +0300

    Added empty MuscleStatus component

---
+++ app/src/components/MuscleStatus.tsx
@@ -0,0 +1,12 @@
+import { ReactElement } from "react";
+import { MuscleGroups } from "../utils/types";
+
+
+export function MuscleStatus(intensityData: MuscleGroups): ReactElement {
+    
+
+
+    return (
+        <div></div>
+    );
+}
