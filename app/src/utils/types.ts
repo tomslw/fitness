6c9b1c6fafbb20c2398151c8e781dff175edd07d
Toms Strals <stralstomslw@gmail.com>
2023-06-27 15:21:47 +0300

    Added MuscleGroups interface and Intensity enum

--- app/src/utils/types.ts
+++ app/src/utils/types.ts
@@ -9,3 +9,22 @@ export interface Excercise {
     addedWeight: number
     // health data goes here, once that has been defined, for now not nessesary
 }
+
+export enum Intensity {
+    none, low, medium, high, injury
+}
+
+export interface MuscleGroups {
+    chest: Intensity;
+    back: Intensity;
+    biceps: Intensity;
+    triceps: Intensity;
+    forearms: Intensity;
+    abdomen: Intensity;
+    gluteus: Intensity;
+    hamstrings: Intensity;
+    quadriceps: Intensity;
+    calves: Intensity;
+    trapezius: Intensity;
+    deltoid: Intensity;
+}
