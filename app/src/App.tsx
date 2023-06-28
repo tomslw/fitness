fdddb0ec3f51a7e46bb846aa2ecd9f603dcf66e4
Toms Strals <stralstomslw@gmail.com>
2023-06-28 15:17:16 +0300

    Frontend: App: Added fix for API handling

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -11,81 +11,77 @@ import { HealthDataList } from './components/HealthDataList';
 
 export function App(): ReactElement {
 
-  const [exercise, setExercise] = useState<Exercise[]>([]);
   const [loading, setLoading] = useState(false);
   const [viewSoreness, setViewSoreness] = useState(true);
 
-  const [healthList, setHealthList] = useState<Array<HealthData>>(new Array<HealthData>({
-    weight: 60,
-    height: 180,
-    morningMuscleFatigue: {
-      chest: Intensity.high,
-      back: Intensity.low,
-      biceps: Intensity.low,
-      triceps: Intensity.low,
-      forearms: Intensity.low,
-      abdomen: Intensity.low,
-      gluteus: Intensity.medium,
-      hamstrings: Intensity.low,
-      quadriceps: Intensity.low,
-      calves: Intensity.low,
-      trapezius: Intensity.low,
-      deltoid: Intensity.low,
-    },
-    diet: new Array<Meal>({    
-      title: "pica",
-      description: "neveseligs ediens",
-      calories: 5000,
-      fat: 100,
-      carbohydrates: 101,
-      protein: 20,
-    },
-    {    
-      title: "Hamburgier",
-      description: "amerikano",
-      calories: 500,
-      fat: 80,
-      carbohydrates: 150,
-      protein: 15,
-    }),
-    workout: new Array<Exercise>({
-      title: "pushups",
-      description: "just do it",
-      restInterval: 2,
-      repetitions: 10,
-      addedWeight: 0,
-      targetMuscles: {
-        chest: Intensity.high,
-        back: Intensity.low,
-        biceps: Intensity.medium,
-        triceps: Intensity.low,
-        forearms: Intensity.low,
-        abdomen: Intensity.low,
-        gluteus: Intensity.none,
-        hamstrings: Intensity.none,
-        quadriceps: Intensity.none,
-        calves: Intensity.none,
-        trapezius: Intensity.low,
-        deltoid: Intensity.medium,
-      }
-    }),
-    caloriesSpent: 2000,
-    dateTime: new Date(2023, 4, 8, 10, 13, 41, 12),
-  }))
+  const [healthList, setHealthList] = useState<Array<HealthData>>(new Array<HealthData>())
 
   // selected HealthData entry
-  const [health, setHealth] = useState<HealthData> (healthList[0]);
-
-  // TODO: add a useEffect that updates the selected HealthData entrie in the list (that doesn't yet exist as im writing this)
+  const [health, setHealth] = useState<HealthData> ({
+    idhd: -1,
+    weight: 0,
+    height: 0,
+    muscleGroups: {
+      chest: Intensity.none,
+      back: Intensity.none,
+      biceps: Intensity.none,
+      triceps: Intensity.none,
+      forearms: Intensity.none,
+      abdomen: Intensity.none,
+      gluteus: Intensity.none,
+      hamstrings: Intensity.none,
+      quadriceps: Intensity.none,
+      calves: Intensity.none,
+      trapezius: Intensity.none,
+      deltoid: Intensity.none,
+    },
+    diet: new Array<Meal>(),
+    workout: new Array<Exercise>(),
+    caloriesSpent: 0,
+    date: new Date(1999, 1, 1, 1, 1, 1, 1),
+  });
 
   useEffect(() => {
     setLoading(true);
     // having the response beeing handeled here is kind of meh, 
     // should probably have a seperate Requester.ts with all the safety checks in the world
-    fetch('exercise/showAll')
+    fetch('healthData/showAll')
       .then(response => response.json())
       .then(data => {
-        setExercise(data);
+        console.log(data);
+        
+        var nonWeirdData: Array<HealthData> = new Array<HealthData>();
+        data.forEach((entry: { idhd: any; weight: any; height: any; muscleGroups: { chest: string; back: string; biceps: string; triceps: string; forearms: string; abdomen: string; gluteus: string; hamstrings: string; quadriceps: string; calves: string; trapezius: string; deltoid: string; }; diet: any; workout: any; caloriesSpent: any; date: string | number | Date; }) => {
+          nonWeirdData.push(
+            {
+              idhd: entry.idhd,
+              weight: entry.weight,
+              height: entry.height,
+              muscleGroups: {
+                chest: Intensity[entry.muscleGroups.chest as keyof typeof Intensity],
+                back: Intensity[entry.muscleGroups.back as keyof typeof Intensity],
+                biceps: Intensity[entry.muscleGroups.biceps as keyof typeof Intensity],
+                triceps: Intensity[entry.muscleGroups.triceps as keyof typeof Intensity],
+                forearms: Intensity[entry.muscleGroups.forearms as keyof typeof Intensity],
+                abdomen: Intensity[entry.muscleGroups.abdomen as keyof typeof Intensity],
+                gluteus: Intensity[entry.muscleGroups.gluteus as keyof typeof Intensity],
+                hamstrings: Intensity[entry.muscleGroups.hamstrings as keyof typeof Intensity],
+                quadriceps: Intensity[entry.muscleGroups.quadriceps as keyof typeof Intensity],
+                calves: Intensity[entry.muscleGroups.calves as keyof typeof Intensity],
+                trapezius: Intensity[entry.muscleGroups.trapezius as keyof typeof Intensity],
+                deltoid: Intensity[entry.muscleGroups.deltoid as keyof typeof Intensity],
+              },
+              diet: entry.diet,
+              workout: entry.workout,
+              caloriesSpent: entry.caloriesSpent,
+              date: new Date(entry.date),
+            }
+          )
+        });
+
+
+        setHealthList(nonWeirdData);
+        setHealth(nonWeirdData[0]);
         setLoading(false);
       })
   }, []);
@@ -100,7 +96,7 @@ export function App(): ReactElement {
     <div className="App">
       <header className="App-header">
         <div className="health-data-list">
-          {/* <HealthDataList healthDataList={healthList} setHealthDataList={} setSelectedItem={setHealth}  /> */}
+          <HealthDataList healthDataList={healthList} setHealthDataList={() => {}} setSelectedItem={setHealth}  />
         </div>
         <div className="health-data-side">
           <HealthSummary healthData={health} setHealthData={setHealth}/>
@@ -110,7 +106,7 @@ export function App(): ReactElement {
           <div className="title">{viewSoreness ? "Muscle soreness" : "Workout routine"}</div>
           <Button variant="contained" onClick={() => setViewSoreness(prevState => !prevState)}> Switch views </Button>
           { viewSoreness ?
-          <MuscleStatus enableEdit={true} intensityData={health.morningMuscleFatigue} setIntensityData={(data => setHealth({ ...health, morningMuscleFatigue: data }))}/>
+          <MuscleStatus enableEdit={true} intensityData={health.muscleGroups} setIntensityData={(data => setHealth({ ...health, muscleGroups: data }))}/>
           :
           <WorkoutList workouts={health.workout} setWorkouts={(data) => setHealth({ ...health, workout: data })} />
           }
