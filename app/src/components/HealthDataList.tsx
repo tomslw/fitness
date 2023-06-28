b9a4e72b370df55df6cc6ab6c36733a2dc5f8c24
Toms Strals <stralstomslw@gmail.com>
2023-06-28 15:18:50 +0300

    Frontend: HealthDataList: Prepping for API changes

--- app/src/components/HealthDataList.tsx
+++ app/src/components/HealthDataList.tsx
@@ -8,7 +8,7 @@ import Button from "@mui/material/Button";
 interface Props {
     healthDataList: Array<HealthData>,
     setHealthDataList: (newData: Array<HealthData>) => void,
-    setSelectedItem: (newSelection: number) => void,
+    setSelectedItem: (newSelection: HealthData) => void,
 }
 
 export function HealthDataList({healthDataList, setHealthDataList, setSelectedItem} : Props): ReactElement {
@@ -17,31 +17,33 @@ export function HealthDataList({healthDataList, setHealthDataList, setSelectedIt
         // get request to backend to get a fresh entry, i need the idmg
         // fresh HealthData entry AND a MuscleGroups entry
 
-        setHealthDataList([...healthDataList, {
-            weight: 60,
-            height: 180,
-            morningMuscleFatigue: {
-              chest: Intensity.none,
-              back: Intensity.none,
-              biceps: Intensity.none,
-              triceps: Intensity.none,
-              forearms: Intensity.none,
-              abdomen: Intensity.none,
-              gluteus: Intensity.none,
-              hamstrings: Intensity.none,
-              quadriceps: Intensity.none,
-              calves: Intensity.none,
-              trapezius: Intensity.none,
-              deltoid: Intensity.none,
-            },
-            diet: new Array<Meal>(),
-            workout: new Array<Exercise>(),
-            caloriesSpent: 0,
-            dateTime: new Date(),
-          }]);
-        setSelectedItem(healthDataList.length);
+        // setHealthDataList([...healthDataList, {
+        //     weight: 60,
+        //     height: 180,
+        //     muscleGroups: {
+        //       chest: Intensity.none,
+        //       back: Intensity.none,
+        //       biceps: Intensity.none,
+        //       triceps: Intensity.none,
+        //       forearms: Intensity.none,
+        //       abdomen: Intensity.none,
+        //       gluteus: Intensity.none,
+        //       hamstrings: Intensity.none,
+        //       quadriceps: Intensity.none,
+        //       calves: Intensity.none,
+        //       trapezius: Intensity.none,
+        //       deltoid: Intensity.none,
+        //     },
+        //     diet: new Array<Meal>(),
+        //     workout: new Array<Exercise>(),
+        //     caloriesSpent: 0,
+        //     date: new Date(),
+        //   }]);
+        setSelectedItem(healthDataList[healthDataList.length]);
         // sort by date tho
     };
+
+    console.log(healthDataList[0]);
 
     return (
         <>
@@ -50,8 +52,8 @@ export function HealthDataList({healthDataList, setHealthDataList, setSelectedIt
                 {
                     healthDataList.map((value, index) =>
                     // wont actually save it locally in the list tho, is basically going to be a local variable
-                    <div className="health-item" onClick={() => setSelectedItem(index)}>
-                        <div className="health-name">{value.dateTime.toLocaleDateString()}</div>
+                    <div className="health-item" onClick={() => setSelectedItem(healthDataList[index])}>
+                        <div className="health-name">{value.date.toLocaleDateString()}</div>
                     </div>
                     )
                 }
