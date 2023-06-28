1073a03c287e95eb2dc39b26bb58b1e3ae8817d2
NiklavsBergs <bergs.niklavs@gmail.com>
2023-06-28 17:33:21 +0300

    Added: MuscleGroupService Functions

--- src/main/java/lv/venta/fitness/services/IMuscleGroupsService.java
+++ src/main/java/lv/venta/fitness/services/IMuscleGroupsService.java
@@ -3,8 +3,8 @@ package lv.venta.fitness.services;
 import lv.venta.fitness.models.MuscleGroups;
 
 public interface IMuscleGroupsService {
-	MuscleGroups getByExerciseId(long idex);
-	MuscleGroups getByHealthDataId(long idhe);
-	void editByExerciseId(long idex);
-	void editByHealthDataId(long idhe);
+	MuscleGroups getByExerciseId(long idex) throws Exception;
+	MuscleGroups getByHealthDataId(long idhe) throws Exception;
+	void editByExerciseId(long idex, MuscleGroups data) throws Exception;
+	void editByHealthDataId(long idhe, MuscleGroups data) throws Exception;
 }
