c3feaea8a01d5d38b7c48c364d72969e08fd5a74
Toms Strals <stralstomslw@gmail.com>
2023-06-29 11:47:20 +0300

    Frontend: DataList: Added check on add entry

--- app/src/components/HealthDataList.tsx
+++ app/src/components/HealthDataList.tsx
@@ -3,7 +3,17 @@ import { Exercise, HealthData, HealthDataShort, Intensity, Meal } from "../utils
 import "./HealthDataList.css"
 
 import Button from "@mui/material/Button";
+import Snackbar from '@mui/material/Snackbar';
+import MuiAlert, { AlertProps } from '@mui/material/Alert';
+import React from "react";
 
+
+const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(
+    props,
+    ref,
+  ) {
+    return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+  });
 
 interface Props {
     healthDataList: Array<HealthDataShort>,
@@ -13,10 +23,18 @@ interface Props {
 
 export function HealthDataList({healthDataList, setHealthDataList, setSelectedItem} : Props): ReactElement {
 
+    const [open, setOpen] = React.useState(false);
+
     const addEntry = useCallback(() => {
         // TODO: maybe check if theres an entry for todays date or not
 
-        fetch('healthData/getFresh')
+        const todayFilter = healthDataList.filter((item) => {
+            var leDate = item.date;
+            var now = new Date();
+            return (leDate.getDate() === now.getDate() && leDate.getMonth() === now.getMonth() && leDate.getFullYear() === now.getFullYear());
+        })
+        if (todayFilter.length === 0) {
+            fetch('healthData/getFresh')
             .then(response => response.json())
             .then(data => {
                 setSelectedItem(data.idhe);
@@ -27,6 +45,10 @@ export function HealthDataList({healthDataList, setHealthDataList, setSelectedIt
                 ...healthDataList,
                 ])
             });
+        } else {
+            setOpen(true);
+        }
+
     }, [healthDataList, setHealthDataList, setSelectedItem]);
 
     console.log(healthDataList[0]);
@@ -44,6 +66,11 @@ export function HealthDataList({healthDataList, setHealthDataList, setSelectedIt
                     )
                 }
             </div>
+            <Snackbar open={open} autoHideDuration={6000} onClose={() => setOpen(false)}>
+                <Alert onClose={() => setOpen(false)} severity="error" sx={{ width: '100%' }}>
+                    There's already an entry for today!
+                </Alert>
+            </Snackbar>
         </>
     );
 }
